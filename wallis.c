0ef2775edd1d77e557dd0f4550e2bcafa9bdd4dc
viscous300 <85053081+viscous300@users.noreply.github.com>
2021-06-04 12:29:12 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,7 +4,15 @@
 #include <math.h>
 
 float wallis_pi(int);
-
+float wallis_pi(int n)
+  { float p=0;
+  for(int i=1;i<=n ;i++)
+  {float e=4*i*i;
+  float k=e/(e-1);
+  p=p+k;
+  }return p*2;
+  
+}
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
